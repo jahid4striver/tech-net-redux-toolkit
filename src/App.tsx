b3d202b1bd47480660560a1845138768364d052c
jahid4striver <jahidhassan701@gmail.com>
2023-08-16 10:08:33 +0600

    Counter created with redux toolkit

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,37 @@
+import { useDispatch, useSelector } from "react-redux"
+import { RootState } from "./redux/store"
+import { decrement, icrementByNumber, increment } from "./redux/features/counter/counterSlice";
 
 
 function App() {
-
-
+  const { count } = useSelector((state: RootState) => state.counter);
+  const dispatch=useDispatch();
   return (
-   <h1>Hello world</h1>
+
+    <div className="flex flex-col items-center mt-10">
+      <h1 className="text-3xl mb-2">Counter App</h1>
+      <div className="flex items-center space-x-4"
+     >
+        <button
+          className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded"
+          onClick={()=>dispatch(decrement())}>
+          -Dec
+        </button>
+        <span className="text-xl">{count}</span>
+        <button
+          className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded"
+          onClick={()=>dispatch(increment())}
+        >
+          +Inc
+        </button>
+        <button
+          className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded"
+          onClick={()=>dispatch(icrementByNumber(5))}
+        >
+          +Inc By Num
+        </button>
+      </div>
+    </div>
   )
 }
 
---
+++ src/routes/index.tsx
@@ -0,0 +1,23 @@
+import { createBrowserRouter } from 'react-router-dom'
+import Home from '../pages/Home'
+import App from '../App';
+import Login from '../pages/Login';
+import NotFound from '../pages/NotFound';
+
+const routes = createBrowserRouter([
+    {
+        path: '/',
+        element: <App />
+    },
+    {
+        path: '/login',
+        element: <Login />
+    },
+    {
+        path: '*',
+        element: <NotFound />
+    },
+
+])
+
+export default routes;
